fe2eadc899f937eac309c85dff21e0cafed9895b
Nazmul Hasan <nazmul.tex99@gmailcom>
2023-10-15 08:16:10 -0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,38 +25,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Dispose extends StatefulWidget {
-  const Dispose({super.key});
-
-  @override
-  State<Dispose> createState() => _DisposeState();
-}
-
-class _DisposeState extends State<Dispose> {
-  final TextEditingController te = TextEditingController();
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    te.dispose();
-    super.dispose();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-        Form(
-          child: TextFormField(
-            controller: te,
-          ),
-        ),
-        ElevatedButton(onPressed: (){
-
-        }, child: const Text("Add"))
-        ],
-      ),),
-    );
-  }
-}
-
